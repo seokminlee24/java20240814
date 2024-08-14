bd883f45379c07a34a4c56cc0eb56d960fae380f
seokminlee24 <lsm67286564@gmail.com>
2024-08-14 17:02:30 +0900

    변수 선언

--- src/ch02/lectrue/p02type/C02BooleanType.java
+++ src/ch02/lectrue/p02type/C02BooleanType.java
@@ -1,20 +1,26 @@
-//package ch02.lectrue.p02type;
-//
-//public class C02BooleanType {
-//    public static void main(String[] arg){
-//        int a = 1;
-//        int b = 2;
-//
-//        if (a){
-//            System.out.println("참일 떄 실행1");
-//        }else {
-//            System.out.println("참일 떄 실행1");
-//        }
-//
-//        if(b){
-//            System.out.println("참일 떄 실행2");
-//        }else {
-//            System.out.println("참일 떄 실행2");
-//        }
-//    }
-//}
+package ch02.lectrue.p02type;
+
+public class C02BooleanType {
+    public static void main(String[] arg){
+        // boolean (1byte)
+        // true, false
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+        if (a) {
+            System.out.println("참일 때 실행1");
+        } else {
+            System.out.println("거짓일 때 실행1");
+        }
+
+        if (b) {
+            System.out.println("참일 때 실행2");
+        } else {
+            System.out.println("거짓일 때 실행2");
+        }
+    }
+}
