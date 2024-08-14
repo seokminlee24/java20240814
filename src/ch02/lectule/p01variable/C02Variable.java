2c911401f74d920f5e26e9f3eb0bc14b134068c3
seokminlee24 <lsm67286564@gmail.com>
2024-08-14 14:42:56 +0900

    변수 선언

---
+++ src/ch02/lectule/p01variable/C02Variable.java
@@ -0,0 +1,22 @@
+package ch02.lectule.p01variable;
+
+public class C02Variable {
+    public static void main(String[] args){
+        //변수명 작성 관습
+        //소문자
+        int age;
+        int Age; //x
+        int AGe; //x
+        int AGE; //x
+        int aGE; //x
+
+        // 여러 단어 조합 시 각 단어 앞만 대문자(처음은 무조건 소문자)
+        int numberOFStudents;
+        int number_of_students; // snake-case(파이썬 관습)
+
+        //lowerCamelCase(자바 변수명 관습)
+        //UpperCamelCase (자바 클래스명 관습)
+
+        //kebab-case(기타 등등)
+    }
+}
