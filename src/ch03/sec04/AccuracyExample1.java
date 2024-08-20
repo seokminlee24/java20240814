dca3641907bfd31cebde2e84d88900c8fce85967
seokminlee24 <lsm67286564@gmail.com>
2024-08-20 11:00:36 +0900

    infinity, NaN

--- src/ch03/sec04/AccuracyExample1.java
+++ src/ch03/sec04/AccuracyExample1.java
@@ -6,7 +6,7 @@ public class AccuracyExample1 {
         double pieceUnit = 0.1;
         int number = 7;
 
-        double result = apple - pieceUnit*number;
-        System.out.println("사과 1개에서 남은양 = " + result);
+        double result = apple - pieceUnit*number; //0.3
+        System.out.println("사과 1개에서 남은양 = " + result);//0.291
     }
 }
