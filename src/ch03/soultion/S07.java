2c5bb63c0fa25b8a4061400033b728762b720431
seokminlee24 <lsm67286564@gmail.com>
2024-08-20 16:20:53 +0900

    문제7

--- src/ch03/soultion/S07.java
+++ src/ch03/soultion/S07.java
@@ -5,7 +5,7 @@ public class S07 {
         double x = 5.0;
         double y = 0.0;
 
-        double z = 5 % y;
+        double z = 5 % y;//NaN
 
         if( Double.isNaN(z) ) {
             System.out.println("0.0으로 나눌 수 없습니다.");
