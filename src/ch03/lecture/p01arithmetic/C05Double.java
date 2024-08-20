c6aeb321e26c8fa2bb2799b408e2504bd2e36cdd
seokminlee24 <lsm67286564@gmail.com>
2024-08-20 10:58:11 +0900

    실수는 근사 값으로 표현됨

--- src/ch03/lecture/p01arithmetic/C05Double.java
+++ src/ch03/lecture/p01arithmetic/C05Double.java
@@ -5,6 +5,6 @@ public class C05Double {
         double a = 0.1;
         double b = 0.2;
         double c = a + b;
-        System.out.println("c = " + c);
+        System.out.println("c = " + c); //0.30000000000000004
     }
 }
