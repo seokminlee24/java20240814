9b98ed351a549d5e18274bddf55efab65767d3f9
seokminlee24 <lsm67286564@gmail.com>
2024-08-20 14:55:42 +0900

    대입 연산자

--- src/ch03/sec10/AssignmentOpreratorExample.java
+++ src/ch03/sec10/AssignmentOpreratorExample.java
@@ -3,15 +3,15 @@ package ch03.sec10;
 public class AssignmentOpreratorExample {
     public static void main(String[] args) {
         int result = 0;
-        result += 10; //10
+        result += 10; //0 + 10  10
         System.out.println("result = " + result);
-        result -= 5; //10-5
+        result -= 5; //10-5 5
         System.out.println("result = " + result);
-        result *= 3; //5*3
+        result *= 3; //5*3 15
         System.out.println("result = " + result);
-        result /= 5; //15/5
+        result /= 5; //15/5 3
         System.out.println("result = " + result);
-        result %= 3; //0 3%3
+        result %= 3; //0 3%3 0
         System.out.println("result = " + result);
     }
 }
