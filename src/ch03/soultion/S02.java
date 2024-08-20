8e11d7f7b4a501433add664244def812b36f31ed
seokminlee24 <lsm67286564@gmail.com>
2024-08-20 15:39:45 +0900

    삼항(조건) 연산자

---
+++ src/ch03/soultion/S02.java
@@ -0,0 +1,9 @@
+package ch03.soultion;
+
+public class S02 {
+    public static void main(String[] args) {
+        int score = 91;
+        String result = (!(score>90)) ? "가" : "나";
+        System.out.println("result = " + result);
+    }
+}
