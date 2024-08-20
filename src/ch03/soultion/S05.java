2fef840def8d2453f58b8e8174f192032c216ac2
seokminlee24 <lsm67286564@gmail.com>
2024-08-20 15:57:16 +0900

    문제5

---
+++ src/ch03/soultion/S05.java
@@ -0,0 +1,11 @@
+package ch03.soultion;
+
+public class S05 {
+    public static void main(String[] args) {
+        int lengthTop = 5;
+        int lengthBottom = 10;
+        int height = 7;
+        double area =(double) (lengthTop+lengthBottom)*7/2;
+        System.out.println("area = " + area); //52.5
+    }
+}
