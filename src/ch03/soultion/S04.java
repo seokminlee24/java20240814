02d3e385a7b3b3f52c7982d000d7943b97d51be0
seokminlee24 <lsm67286564@gmail.com>
2024-08-20 15:50:58 +0900

    문제4

---
+++ src/ch03/soultion/S04.java
@@ -0,0 +1,9 @@
+package ch03.soultion;
+
+public class S04 {
+    public static void main(String[] args) {
+        int value = 356;
+        System.out.println(value - 56);
+        System.out.println(value - value%300);
+    }
+}
