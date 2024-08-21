ba2930adf9a5269419d06c3e2111b122f419740e
seokminlee24 <lsm67286564@gmail.com>
2024-08-21 09:41:34 +0900

    else if문

---
+++ src/ch04/lecture/p01if/C02/C03ElseIf.java
@@ -0,0 +1,20 @@
+package ch04.lecture.p01if.C02;
+
+public class C03ElseIf {
+    public static void main(String[] args) {
+        System.out.println(1);
+        if(false){
+            System.out.println(2);
+            System.out.println(3);
+        }else if(true){
+            System.out.println(4);
+            System.out.println(5);
+        } else if (true) {
+            System.out.println(6);
+            System.out.println(7);
+        }else { //마지막에 있어야한다
+            System.out.println(8);
+        }
+        System.out.println(9);
+    }
+}
