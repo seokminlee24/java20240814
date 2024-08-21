43ef29a481029978277780e97ffcd1b4600c3a98
seokminlee24 <lsm67286564@gmail.com>
2024-08-21 17:12:00 +0900

    for_x와 y값 구하기

---
+++ src/ch04/sol/S06.java
@@ -0,0 +1,12 @@
+package ch04.sol;
+
+public class S06 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j < i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
