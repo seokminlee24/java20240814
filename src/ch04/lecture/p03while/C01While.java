506d206d947c8d23b445a7983e6a1634c865a39f
seokminlee24 <lsm67286564@gmail.com>
2024-08-21 16:28:16 +0900

    while-2,3 3번반복

---
+++ src/ch04/lecture/p03while/C01While.java
@@ -0,0 +1,13 @@
+package ch04.lecture.p03while;
+
+public class C01While {
+    public static void main(String[] args) {
+        System.out.println(1);
+
+        while (true){
+            System.out.println(2);
+            System.out.println(3);
+        }
+        //System.out.println(4);
+    }
+}
