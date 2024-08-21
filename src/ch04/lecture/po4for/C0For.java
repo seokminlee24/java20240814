76c7de91f60aabd446c4a9ec157cab471659174a
seokminlee24 <lsm67286564@gmail.com>
2024-08-21 16:42:04 +0900

    for

---
+++ src/ch04/lecture/po4for/C0For.java
@@ -0,0 +1,12 @@
+package ch04.lecture.po4for;
+
+public class C0For {
+    public static void main(String[] args) {
+        //int i = 0;
+
+        for (int i = 0; i < 3; i++){
+            //명령문들 .....
+            System.out.println("명령문");
+        }
+    }
+}
