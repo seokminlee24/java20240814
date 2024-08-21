a50576774ff3fd55d3fd0718efc164a8b4dea2be
seokminlee24 <lsm67286564@gmail.com>
2024-08-21 16:41:19 +0900

    while_1~10

---
+++ src/ch04/sec05/PrintFrom1To10Example.java
@@ -0,0 +1,13 @@
+package ch04.sec05;
+
+public class PrintFrom1To10Example {
+    public static void main(String[] args) {
+        // i ~ 10까지 각각 출력
+        int i = 1;
+        while (i <= 10){
+            System.out.print(i + "  ");
+            i++;
+        }
+
+    }
+}
