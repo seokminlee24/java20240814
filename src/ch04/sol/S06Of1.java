b6c60505614d9394803a1f179437931bed937bac
seokminlee24 <lsm67286564@gmail.com>
2024-08-22 11:55:39 +0900

    for문

--- src/ch04/sol/S06Of1.java
+++ src/ch04/sol/S06Of1.java
@@ -9,6 +9,7 @@ public class S06Of1 {
         3
         4
          */
+        System.out.println("i1");
         for (int i = 0; i < 5; i++) {
             System.out.println(i);
         }
@@ -22,7 +23,8 @@ public class S06Of1 {
         1
         0
          */
-        for (int i = 4; i >= 0; i--) {
+        System.out.println("i2");
+        for (int i = 4; i > 0; i--) {
             System.out.println(i);
         }
 
@@ -34,7 +36,8 @@ public class S06Of1 {
         4
         5
          */
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("i3");
+        for (int i = 0; i <= 5; i++) {
             System.out.println(i);
         }
 
@@ -48,10 +51,11 @@ public class S06Of1 {
         2
         1
          */
+        System.out.println("i4");
         for (int i = 5; i >= 1; i--) {
             System.out.println(i);
         }
-
+        System.out.println("--------------");
         /*
         0
         01
@@ -59,14 +63,15 @@ public class S06Of1 {
         0123
         01234
          */
-        System.out.println("--------------");
+        System.out.println("i5");
         for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < (i + 1); j++) {
+            for (int j = 0; j < i+1; j++) {
                 System.out.print(j);
             }
             System.out.println();
         }
 
+        System.out.println("----------------");
         /*
         1
         12
@@ -74,14 +79,14 @@ public class S06Of1 {
         1234
         12345
          */
-        System.out.println("----------------");
-        for (int i = 0; i < 5; i++) {
-            for (int j = 1; j <= (i + 1); j++) {
+        System.out.println("i6");
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 1; j < (i+1); j++) {
                 System.out.print(j);
             }
             System.out.println();
         }
-
+        System.out.println("-----------");
         /*
 
          *****
@@ -91,14 +96,14 @@ public class S06Of1 {
          *
 
          */
-        System.out.println("-----------");
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < (5 - i); j++) {
+        System.out.println("i7");
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 1; j < (6-i) ; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-
+        System.out.println("--------------");
         /*
 
         0
@@ -108,12 +113,12 @@ public class S06Of1 {
         01234
 
          */
-        System.out.println("--------------");
+        System.out.println("i8");
         int i = 0;
 
-        for (int j = 0; j < 5; j++) {
-            for (int k = 0; k <= j; k++) {
-                System.out.print(i % 10);
+        for (int j = 0; j <= 5; j++) {
+            for (int k = 0; k < j; k++) {
+                System.out.print( i%10);
                 i++;
             }
             System.out.println();
