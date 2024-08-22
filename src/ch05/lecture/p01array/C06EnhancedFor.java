d912cc7e30c5858b2da634dd085ec29d1abdc203
seokminlee24 <lsm67286564@gmail.com>
2024-08-22 14:46:29 +0900

    배열-향상된 for문

---
+++ src/ch05/lecture/p01array/C06EnhancedFor.java
@@ -0,0 +1,19 @@
+package ch05.lecture.p01array;
+
+public class C06EnhancedFor {
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        arr[0] = 99;
+        arr[1] = 88;
+        arr[2] = 77;
+
+        for (int i = 0; i < arr.length ; i++) {
+            System.out.println("arr[i] = " + arr[i]);
+        }
+        System.out.println("EnhancedFor=================");
+        //향상된 for
+        for(int n : arr){
+            System.out.println("n = " + n);
+        }
+    }
+}
