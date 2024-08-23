7668ce5356d294831b291cd5ca4afedb24227e54
seokminlee24 <lsm67286564@gmail.com>
2024-08-23 12:35:46 +0900

    Parameter

---
+++ src/ch05/lecture/p02reference/C05Parameter.java
@@ -0,0 +1,13 @@
+package ch05.lecture.p02reference;
+
+public class C05Parameter {
+    public static void main(String[] args) {
+        int b = 5;
+
+        method1(b);
+    }
+
+    public static void method1(int a) {
+        System.out.println(a);
+    }
+}
---
+++ src/ch05/lecture/p02reference/C07Parameter.java
@@ -0,0 +1,22 @@
+package ch05.lecture.p02reference;
+
+public class C07Parameter {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {3, 4},
+                {5, 7}
+        };
+        System.out.println("1:" + arr[0][0]);// 3
+        System.out.println("5:" + arr[1][0]);//5
+        method1(arr);
+        System.out.println("4:" + arr[0][0]);// 33
+
+
+    }
+
+    public static void method1(int[][] b) {
+        System.out.println("2:" + b[0][0]); // 3
+        b[0] = new int[]{33, 44};
+        System.out.println("3:" + b[0][0]); // 33
+    }
+}
