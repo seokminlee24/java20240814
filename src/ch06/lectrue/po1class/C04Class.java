2060b1a8ae2528eb8bd4900c576240a8e9f29d2e
seokminlee24 <lsm67286564@gmail.com>
2024-08-26 15:58:21 +0900

    field와 methods

---
+++ src/ch06/lectrue/po1class/C04Class.java
@@ -0,0 +1,24 @@
+package ch06.lectrue.po1class;
+
+public class C04Class {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Class4{
+    //field
+    //값이 무엇인지?
+    int age = 3;
+    String name ="모니터";
+
+    //method
+    //기능은 무엇인가?
+    public void  on(){
+        System.out.println("키다");
+    }
+
+    public void  off(){
+        System.out.println("끄다");
+    }
+}
