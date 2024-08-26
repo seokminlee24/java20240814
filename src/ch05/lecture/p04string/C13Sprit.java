4b1ac36b08da11f968d4bad41cf0d8594e4580fa
seokminlee24 <lsm67286564@gmail.com>
2024-08-26 12:04:05 +0900

    Split

--- src/ch05/lecture/p04string/C13Sprit.java
+++ src/ch05/lecture/p04string/C13Sprit.java
@@ -3,17 +3,35 @@ package ch05.lecture.p04string;
 public class C13Sprit {
     public static void main(String[] args) {
         //문자열을 나누는 메소드
-        String s = "java, css,spring,react";
-        String[] t= s.split(",");
-        System.out.println("t[0] = " + t[0]);
-        System.out.println("t[1] = " + t[1]);
-        System.out.println("t[2] = " + t[2]);
-        System.out.println("t[3] = " + t[3]);
+        // split : 문자열을 나누는 메소드
+        String s = "java,css,spring,react";
+        String[] t = s.split(",");
+        System.out.println(t[0]); // java
+        System.out.println(t[1]); // css
+        System.out.println(t[2]); // spring
+        System.out.println(t[3]); // react
 
         String u = "java css html";
-        String[] v= u.split(",");
-        System.out.println("v[0] = " + v[0]);
-        System.out.println("v[1] = " + v[2]);
-        System.out.println("v[2] = " + v[4]);
+        String[] v = u.split(" ");
+        System.out.println(v[0]); // java
+        System.out.println(v[1]); // css
+        System.out.println(v[2]); // html
+
+        String w = "java css";
+        String[] x = w.split("");
+        System.out.println(x[0]); //j
+        System.out.println(x[3]); //a
+        System.out.println(x[4]); // " "
+        System.out.println(x[7]); // s
+
+        String y = "java  css";
+        String[] z = y.split(" ");
+        System.out.println(z[0]); // java
+        System.out.println(z[1]); // ""
+        System.out.println(z[2]); // css
+
+        String[] a = y.split(" +"); // 정규표현식
+        System.out.println(a[0]); // java
+        System.out.println(a[1]); // css
     }
 }
