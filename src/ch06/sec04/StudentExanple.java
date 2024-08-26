04226953f5dcd46bee382b5e6df7a46b735a1e24
seokminlee24 <lsm67286564@gmail.com>
2024-08-26 16:07:45 +0900

    클래스명 작성관습

--- src/ch06/sec04/StudentExanple.java
+++ src/ch06/sec04/StudentExanple.java
@@ -4,7 +4,7 @@ public class StudentExanple {
     public static void main(String[] args) {
         Student s1 = new Student();
         System.out.println("s1 변수가 Student 객체를 참조합니다.");
-        System.out.println("s1 객체 생성.");/
+        System.out.println("s1 객체 생성.");
 
         Student s2 = new Student();
         System.out.println("s2 변수가 Student 객체를 참조합니다.");
