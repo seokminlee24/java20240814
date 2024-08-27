1fd4fa9a0b31d9590ae09167c6cc573bcec4bc99
seokminlee24 <lsm67286564@gmail.com>
2024-08-27 12:37:52 +0900

    확인문제13

--- src/ch06/sol/S13.java
+++ src/ch06/sol/S13.java
@@ -2,7 +2,15 @@ package ch06.sol;
 
 public class S13 {
     public static void main(String[] args) {
-
+        Member member = new Member();
+        member.Name = "이석민";
+        System.out.println("member.Name = " + member.Name);
+        member.id= "hong";
+        System.out.println("member.id = " + member.id);
+        member.password = "1234";
+        System.out.println("member.password = " + member.password);
+        member.age = 10;
+        System.out.println("member.age = " + member.age);
     }
 }
 class Member{
