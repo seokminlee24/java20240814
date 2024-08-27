4a818e90f2fa548837a1b34cbe1a111f479dcf22
seokminlee24 <lsm67286564@gmail.com>
2024-08-27 11:41:39 +0900

    Member 클래스 선언

---
+++ src/ch06/sol/S13.java
@@ -0,0 +1,13 @@
+package ch06.sol;
+
+public class S13 {
+    public static void main(String[] args) {
+
+    }
+}
+class Member{
+    String Name;
+    String id;
+    String password;
+    int age;
+}
