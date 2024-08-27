7bb5488e234d2f8186af63a4e12ef2fcb75f8606
seokminlee24 <lsm67286564@gmail.com>
2024-08-27 10:21:02 +0900

    varargs

---
+++ src/ch06/lectrue/p03method/C08Varargs.java
@@ -0,0 +1,38 @@
+package ch06.lectrue.p03method;
+
+import java.util.Arrays;
+
+public class C08Varargs {
+    public static void main(String[] args) {
+        //varargs(variable arguments, 가변길이 매개변수):
+        //변수
+        MyClass08 o = new MyClass08();
+        o.method2();
+        o.method2(1);
+        o.method2(10, 20);
+        o.method2(5, 6, 7, 7, 1, 1, 1, 1, 1);
+
+        System.out.println();
+        o.method3();
+        o.method3(6, 7, 8);
+        o.method3(10, 20, 30, 40, 50);
+    }
+}
+class MyClass08{
+    public void  method1(){
+        System.out.println("같은일");
+    }
+    public void  method1(int a){
+        System.out.println("같은일");
+    }
+    public void  method1(int a,int b){
+        System.out.println("같은일");
+    }
+    public void  method2(int...a){
+        System.out.println("메소드 2 할일...");
+    }
+    public void  method3(int...a){
+        //a를 배열로 사용 가능
+        System.out.println(Arrays.toString(a));
+    }
+}
---
+++ src/ch06/lectrue/p03method/C09Varargs.java
@@ -0,0 +1,15 @@
+package ch06.lectrue.p03method;
+
+public class C09Varargs {
+    public static void main(String[] args) {
+    }
+}
+class MyClass09{
+    //Varargs 를 다른 타입과 썩어 쓸 떈 가장 마지막에 작성
+    public  void  method1(String n, int...a){
+
+    }
+    //public  void  method1(int...a, String n){
+    //
+    //    }
+}
