4b33c7e8cab92eb0a54024166090388fa6cb82b0
seokminlee24 <lsm67286564@gmail.com>
2024-08-28 12:23:57 +0900

    패키지

--- src/ch06/lectrue/p07packge/C02packge.java
+++ src/ch06/lectrue/p07packge/C02packge.java
@@ -2,11 +2,8 @@ package ch06.lectrue.p07packge;
 
 public class C02packge {
     public static void main(String[] args) {
-        // 다른 패키지에 있는 클래스 사용 시 패키지명까지 명시
-        ch06.lecture.p07package.package1.MyClass01 o1
-                = new ch06.lecture.p07package.package1.MyClass01();
-
+        ch06.lectrue.p07packge.packge01.MyClass01 o1
+                = new ch06.lectrue.p07packge.packge01.MyClass01();
         System.out.println(o1);
-
     }
 }
