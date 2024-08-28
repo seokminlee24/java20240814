292b356d7a81bfe0457722fd3ad4b1b95a636b20
seokminlee24 <lsm67286564@gmail.com>
2024-08-28 11:58:54 +0900

    static final

---
+++ src/ch06/sol/s17/Printer.java
@@ -0,0 +1,19 @@
+package ch06.sol.s17;
+
+public class Printer {
+    static void println(int a){
+        System.out.println(a);
+    }
+
+    static void println(boolean a){
+        System.out.println(a);
+    }
+
+    static void println(double a){
+        System.out.println(a);
+    }
+
+    static void println(String name){
+        System.out.println(name);
+    }
+}
---
+++ src/ch06/sol/s17/S17.java
@@ -0,0 +1,11 @@
+package ch06.sol.s17;
+
+public class S17 {
+    public static void main(String[] args) {
+        Printer.println(10);
+        Printer.println(true);
+        Printer.println(5.7);
+        Printer.println("홍길동");
+    }
+}
+
