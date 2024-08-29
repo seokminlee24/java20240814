413650c4c519d32407eff53f25a43cf75aa26ec4
seokminlee24 <lsm67286564@gmail.com>
2024-08-29 18:03:43 +0900

    instanceof

---
+++ src/ch07/sec09/Person.java
@@ -0,0 +1,13 @@
+package ch07.sec09;
+
+public class Person {
+    public String name;
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public void wolk(){
+        System.out.println("걷습니다.");
+    }
+}
