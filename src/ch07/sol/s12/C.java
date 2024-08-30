b2a13fc00605c006ca11f5c5bb759353e8321614
seokminlee24 <lsm67286564@gmail.com>
2024-08-30 12:45:18 +0900

    문제12

---
+++ src/ch07/sol/s12/C.java
@@ -0,0 +1,11 @@
+package ch07.sol.s12;
+
+public class C extends A {
+    @Override
+    public void method1() {
+        System.out.println("C-method1()");
+    }
+    public void method2() {
+        System.out.println("C-method2()");
+    }
+}
