4d0de60f7cf330d4e57a911189c1604a927b08fd
seokminlee24 <lsm67286564@gmail.com>
2024-08-30 12:45:01 +0900

    문제10

---
+++ src/ch07/sol/s10/Computer.java
@@ -0,0 +1,19 @@
+package ch07.sol.s10;
+
+public class Computer extends Machine {
+    //해결 방안
+    @Override
+    public void powerOn() {
+        super.powerOn();
+    }
+
+    @Override
+    public void powerOff() {
+        super.powerOff();
+    }
+
+    @Override
+    public void work() {
+
+    }
+}
---
+++ src/ch07/sol/s10/Machine.java
@@ -0,0 +1,7 @@
+package ch07.sol.s10;
+
+public abstract class Machine {
+    public void powerOn(){}
+    public void powerOff(){}
+    public abstract void work();
+}
