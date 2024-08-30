b2c101b8edd1a52f11974ae28f5f380c8edaa8c2
seokminlee24 <lsm67286564@gmail.com>
2024-08-30 12:45:11 +0900

    문제11

---
+++ src/ch07/sol/s11/Activity.java
@@ -0,0 +1,7 @@
+package ch07.sol.s11;
+
+public class Activity {
+    public  void  onCreate(){
+        System.out.println("기본적인 실행 내용");
+    }
+}
---
+++ src/ch07/sol/s11/ActivityExample.java
@@ -0,0 +1,12 @@
+package ch07.sol.s11;
+
+public class ActivityExample {
+    public static void main(String[] args) {
+        MainActivity activity = new MainActivity();
+        activity.onCreate();
+
+        MainActivity mainActivity = new MainActivity();
+        mainActivity.onCreate();
+    }
+
+}
