e0a36be932680735ad89daa62c97c0d6f6c6cdd8
seokminlee24 <lsm67286564@gmail.com>
2024-08-30 14:51:54 +0900

    interface,자동형변환

---
+++ src/ch08/lectrue/p01interface/C02Interface.java
@@ -0,0 +1,22 @@
+package ch08.lectrue.p01interface;
+
+public class C02Interface {
+    public static void main(String[] args) {
+        MyClass11 o1 = new MyClass11();
+        MyClass12 o2 = new MyClass12();
+
+        //자동형변환
+        MyInterface1 o3 = o1;
+        MyInterface1 o4 = o2;
+    }
+}
+
+//interface : 여러 클래스의 객체를 하나의 타입으로 묶음
+
+// @formatter:off
+interface  MyInterface1 {}
+
+//MyClass11 이 MyInterface1을 구현
+//구현클래스(concrete class,구상 클래스, 구체 클래스)
+class  MyClass11 implements MyInterface1{}
+class  MyClass12 implements MyInterface1{}
