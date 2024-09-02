16e1b8aea76b473de413fc6b4351a3ea6be7d993
seokminlee24 <lsm67286564@gmail.com>
2024-09-02 16:47:31 +0900

    문제5

--- src/ch08/sol/S05/RemoconExample.java
+++ src/ch08/sol/S05/RemoconExample.java
@@ -1,19 +1,25 @@
 package ch08.sol.S05;
 
 public class RemoconExample {
+    public static void method(Remocon remocon){
+        System.out.println(remocon.powerOn());
+    }
     public static void main(String[] args) {
-        Remocon r = new Tv();
-        r.powerOn();
+        //Remocon r = new Tv();
+        //r.powerOn();
+        method(new Tv());
     }
 }
 
 interface Remocon{
-    void powerOn();
+    //void powerOn();
+    String  powerOn();
 }
 class Tv implements Remocon{
     @Override
-    public void powerOn() {
-        System.out.println("TV를 켯습니다.");
+    public String powerOn() {
+        //System.out.println("TV를 켯습니다.");
+        return "TV를 켯습니다.";
     }
 }
 
