762587bda5ba8854a58c875d61bc0d12b9f1e0ca
seokminlee24 <lsm67286564@gmail.com>
2024-09-02 10:55:33 +0900

    interface 의 필드는 public static final

---
+++ src/ch08/lectrue/p02etc/C01Constant.java
@@ -0,0 +1,17 @@
+package ch08.lectrue.p02etc;
+
+public class C01Constant {
+
+}
+
+interface MyInterface{
+    //public final static 필드(상수,Constant)
+    int AGE = 0;
+    static String NAME = "SON";
+    final int NUMBER = 999;
+    final static double PI = 3.141592;
+    public final static String MY_ADDRESS = "Korea";
+
+    //public abstract 메소드
+    void method1();
+}
