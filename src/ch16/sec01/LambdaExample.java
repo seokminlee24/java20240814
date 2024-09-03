ea6267f92122d1997cf06182c529fbcd53a43bdb
seokminlee24 <lsm67286564@gmail.com>
2024-09-03 11:59:24 +0900

    람다식

---
+++ src/ch16/sec01/LambdaExample.java
@@ -0,0 +1,41 @@
+package ch16.sec01;
+
+public class LambdaExample {
+    public static void main(String[] args) {
+        //익명클래스 객체
+        Calculable c = new Calculable() {
+            @Override
+            public void calculate(int x, int y) {
+                System.out.println(x * y);
+            }
+        };
+        //객체 참조값 전달
+        action(c);
+
+        // 익명클래스 객체의 참조값 전달
+        action(new Calculable() {
+            @Override
+            public void calculate(int x, int y) {
+                System.out.println(x/y);
+            }
+        });
+
+        //람다 전달
+        action((x, y) -> {
+            int result = x + y;
+            System.out.println("result = " + result);
+        });
+
+        action((x, y) -> {
+            int result = x - y;
+            System.out.println("result = " + result);
+        });
+    }
+    public static void action(Calculable calculable){
+        //데이터
+        int x = 10;
+        int y =4;
+        //데이터 처리
+        calculable.calculate(x,y);
+    }
+}
