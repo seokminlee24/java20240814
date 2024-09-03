1cd8b6f013a88173f0af9599c52b0d262c418e16
seokminlee24 <lsm67286564@gmail.com>
2024-09-03 12:00:20 +0900

    문제7

---
+++ src/ch16/sol/s07/Examples.java
@@ -0,0 +1,26 @@
+package ch16.sol.s07;
+
+public class Examples {
+    private static  int[] scores = {10,50,3};
+
+    public static int maxOrMin(Operator operator) {
+        int result = scores[0];
+        for (int score : scores) {
+            result = operator.apply(result, score);
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int max = maxOrMin((x,y)-> Math.max(x,y));
+        System.out.println("최대값 :"+ max);
+
+        int min = maxOrMin((x, y) -> Math.min(x,y));
+        System.out.println("최소값 :"+ min);
+
+        /*int max = maxOrMin((x,y)-> (x >= y) ? x : y);
+        System.out.println("최대값 :"+ max);
+
+        int min = maxOrMin((x, y) -> (x <= y) ? x : y);
+        System.out.println("최소값 :"+ min);*/
+    }
+}
---
+++ src/ch16/sol/s07/Operator.java
@@ -0,0 +1,5 @@
+package ch16.sol.s07;
+
+public interface Operator {
+    int apply(int x, int y);
+}
