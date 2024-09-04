5c3d33876a5db02a1faffe52b72d43f9c97b79af
seokminlee24 <lsm67286564@gmail.com>
2024-09-04 12:33:00 +0900

    문제4

---
+++ src/ch09/sol/s04/Car.java
@@ -0,0 +1,6 @@
+package ch09.sol.s04;
+
+public class Car{
+    class Tire {}
+    static class Engine{}
+}
---
+++ src/ch09/sol/s04/CarExample.java
@@ -0,0 +1,7 @@
+package ch09.sol.s04;
+
+public class CarExample {
+    Car myCar = new Car();
+    Car.Tire tire = myCar.new Tire();
+    Car.Engine engine = new Car.Engine();
+}
