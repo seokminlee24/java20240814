a51593acd2740bfa8da81965fc4ff85ff47be929
seokminlee24 <lsm67286564@gmail.com>
2024-09-05 17:08:59 +0900

    비교

---
+++ src/ch12/lecture/p02wrapper/C05Wrapper.java
@@ -0,0 +1,13 @@
+package ch12.lecture.p02wrapper;
+
+public class C05Wrapper {
+    public static void main(String[] args) {
+        Integer a = 5000;
+        Integer b = 7000;
+        Integer c = 5000;
+
+        System.out.println(a <= b); // true
+        System.out.println(a <= c); // true
+
+    }
+}
