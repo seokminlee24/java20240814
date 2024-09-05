52a9aa429f764217bcedf20952f560429bf6427f
seokminlee24 <lsm67286564@gmail.com>
2024-09-05 17:15:38 +0900

    문제2

---
+++ src/ch13/so1/s02/Container.java
@@ -0,0 +1,14 @@
+package ch13.so1.s02;
+
+public class Container <T>{
+    public T t;
+
+    public void set(T t) {
+        this.t = t;
+    }
+
+    public T get() {
+        return t;
+    }
+
+}
