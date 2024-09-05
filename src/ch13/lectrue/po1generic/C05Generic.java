4619e2931effb1859d5fb215731326e078bbf0dd
seokminlee24 <lsm67286564@gmail.com>
2024-09-05 17:14:47 +0900

    여러개의 타입 파라미터

---
+++ src/ch13/lectrue/po1generic/C05Generic.java
@@ -0,0 +1,18 @@
+package ch13.lectrue.po1generic;
+
+public class C05Generic {
+
+}
+
+// https://docs.oracle.com/javase/tutorial/java/generics/types.html
+// 타입 파라미터명 작성 관습
+// : 대문자 한글자
+
+// 자주 사용되는 이름
+// E : Element
+// K : Key
+// N : Number
+// T : Type
+// V : Value
+// S, U, V etc. : 2nd, 3rd, 4th types
+// T1, T2, T3 etc. : 1st, 2nd, 3rd... types
