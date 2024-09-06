5f49b2bc1d08cf2f595a0693ae2fa95754c055fd
seokminlee24 <lsm67286564@gmail.com>
2024-09-06 16:53:39 +0900

    문제17

--- src/ch12/sol12/s17/patternMatcherExample.java
+++ src/ch12/sol12/s17/patternMatcherExample.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class patternMatcherExample {
     public static void main(String[] args) {
         String id = "5Angel1004";
-        String regex = "[a-zA-Z][a-zA-Z0-9]{7,11}";
+        String regex = "[0-9a-zA-Z][a-zA-Z0-9]{7,11}";
         boolean match = Pattern.matches(regex, id);
 
         if (match) {
