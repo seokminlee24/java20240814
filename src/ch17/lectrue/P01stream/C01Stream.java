7cd19ac94247285b2391765f9317d8d00a2eb9d9
seokminlee24 <lsm67286564@gmail.com>
2024-09-11 17:08:15 +0900

    stream

---
+++ src/ch17/lectrue/P01stream/C01Stream.java
@@ -0,0 +1,17 @@
+package ch17.lectrue.P01stream;
+
+public class C01Stream {
+    public static void main(String[] args) {
+        //stream : 흘러가는 데이터에 대한 연산(메소드)를 가진 객체
+
+        //package : java.util.stream
+        //Stream,IntStream,DoubleStream,LongStream
+        //
+
+        //1.Stream 만들기
+
+        //2,중간연산
+
+        //3.최종연산
+    }
+}
