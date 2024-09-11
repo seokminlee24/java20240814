8e294ffba17c0d007083f10e9946a957bd75b103
seokminlee24 <lsm67286564@gmail.com>
2024-09-11 17:08:46 +0900

    중간연산, 최종연산

---
+++ src/ch17/lectrue/P02operation/C01Operation.java
@@ -0,0 +1,13 @@
+package ch17.lectrue.P02operation;
+
+public class C01Operation {
+    public static void main(String[] args) {
+        // 스트림 연산(메소드)
+
+        //1.중간 연산(intermediate operation)
+        // return : steam(IntStream, DoubleStream, LongStream)
+
+        //2.최종 연산(terminal operation)
+        //return Stream이 아님
+    }
+}
