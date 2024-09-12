79318858b2d6924e410ca1ebe6ff4fad0741de1e
seokminlee24 <lsm67286564@gmail.com>
2024-09-12 17:52:28 +0900

    reduce

---
+++ src/ch17/sec11/Student.java
@@ -0,0 +1,19 @@
+package ch17.sec11;
+
+public class Student {
+    private String name;
+    private int score;
+
+    public Student(String name, int price) {
+        this.name = name;
+        this.score = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
