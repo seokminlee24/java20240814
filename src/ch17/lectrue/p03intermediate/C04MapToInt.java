1d0bd5ff68e6e763f345fc328ee75f029a0077b5
seokminlee24 <lsm67286564@gmail.com>
2024-09-12 17:33:51 +0900

    map

---
+++ src/ch17/lectrue/p03intermediate/C04MapToInt.java
@@ -0,0 +1,12 @@
+package ch17.lectrue.p03intermediate;
+
+import java.util.List;
+
+public class C04MapToInt {
+    public static void main(String[] args) {
+        List<String> list = List.of("java","css","spring","react","bootstrap");
+        list.stream()
+                .mapToInt(e -> e.length())
+                .forEach(System.out::println);
+    }
+}
