fd7d091c6632c8671a6d782c0ad719a80dcbc693
seokminlee24 <lsm67286564@gmail.com>
2024-09-13 15:01:42 +0900

    exception 발생

---
+++ src/ch11/lectrue/p01exception/C02ArithmeticException.java
@@ -0,0 +1,12 @@
+package ch11.lectrue.p01exception;
+
+public class C02ArithmeticException {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 0;
+
+        int c = a / b; //ArithmeticException
+
+        System.out.println("다음코드");
+    }
+}
---
+++ src/ch11/lectrue/p01exception/C03IndexOutOfBoundsException.java
@@ -0,0 +1,13 @@
+package ch11.lectrue.p01exception;
+
+import java.util.List;
+
+public class C03IndexOutOfBoundsException {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(3,4,5);
+        Integer i1 = list.get(2);
+        Integer i2 = list.get(3); //x
+
+        System.out.println("다음");
+    }
+}
