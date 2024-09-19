6c65bc313f5f3325b80a5ee086b2ead9457296a1
seokminlee24 <lsm67286564@gmail.com>
2024-09-19 17:33:56 +0900

    쓰레드 상채

---
+++ src/ch14/sec03/exam02/BeepPrintExample.java
@@ -0,0 +1,45 @@
+package ch14.sec03.exam02;
+
+import java.awt.*;
+
+public class BeepPrintExample {
+    public static void main(String[] args) {
+        /*Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Toolkit toolkit = Toolkit.getDefaultToolkit();
+                for (int i = 0; i < 5; i++) {
+                    toolkit.beep();
+                    try {
+                        Thread.sleep(500);
+                    }catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });*/
+
+        Thread thread = new Thread(() -> {
+            Toolkit toolkit = Toolkit.getDefaultToolkit();
+            for (int i = 0; i < 5; i++) {
+                toolkit.beep();
+                try {
+                    Thread.sleep(500);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("띵");
+            try {
+                Thread.sleep(500);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
