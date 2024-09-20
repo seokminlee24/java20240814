9623bb6db8b69f68c1a03a0f2f2e03f152781a13
seokminlee24 <lsm67286564@gmail.com>
2024-09-20 17:31:46 +0900

    buffered

---
+++ src/ch18/lectrue/p06buffer/C03Flush.java
@@ -0,0 +1,11 @@
+package ch18.lectrue.p06buffer;
+
+public class C03Flush {
+    public static void main(String[] args) {
+        //flush
+        // : Buffer가 있는 OutputSteam, Writer 에서
+        //버퍼에 담긴 데이터를 목적지로 한번에 전송
+
+
+    }
+}
