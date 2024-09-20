8e02fbe3f1c878eca4155e0e4ce9ed8921b48151
seokminlee24 <lsm67286564@gmail.com>
2024-09-20 17:31:33 +0900

    buffered

---
+++ src/ch18/lectrue/p06buffer/C02Buffered.java
@@ -0,0 +1,26 @@
+package ch18.lectrue.p06buffer;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class C02Buffered {
+    public static void main(String[] args) throws IOException {
+        String name = "C:/Temp/output62.txt";
+
+        OutputStream os = new FileOutputStream(name);
+        OutputStream os2 = new BufferedOutputStream(os);
+
+        byte[] data = new byte[100];
+        long start = System.nanoTime();
+        for (int i = 0; i < 100; i++) {
+            os2.write(data);
+        }
+        long end = System.nanoTime();
+        long time = end - start;
+        System.out.println("time = " + time);
+
+        os2.close();
+    }
+}
